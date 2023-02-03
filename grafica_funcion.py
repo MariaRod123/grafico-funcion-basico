8a530564bb22b3dddb36f3eab804452f8071e32c
María <72228855+MariaRod123@users.noreply.github.com>
2023-02-02 23:39:32 -0300

    Update grafica_funcion.py

--- grafica_funcion.py
+++ grafica_funcion.py
@@ -1,13 +1,10 @@
-import math
 import numpy as np
 from matplotlib import pyplot as plt
-
 
 # ejemplo de construcción de la gráfica para la función e^x
 
 def funcionExponencial(x):
     return np.exp(x)
-
 
 x = np.linspace(0, 10, 100) # los valores representan el inicio del grafico, o sea en x=0, luego el x=10 valor máximo en abscisa, y 100 es la catidad de puntos del gráfico
 plt.plot(x, funcionExponencial(x))
